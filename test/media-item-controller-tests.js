d5bf22f368ad6a3c008de9f1fb99c75b6ab29fde
cheuni02 <inhcheung@gmail.com>
2021-01-04 22:47:31 +0000

    added 'payloads' dir to house all the long json payloads used by the tests

--- test/media-item-controller-tests.js
+++ test/media-item-controller-tests.js
@@ -1,5 +1,7 @@
 const common = '../lib/common';
 const testrailApi = '../lib/test-rail-api';
+const payloadsUrl = './payloads/media-item-controller-test-payloads';
+const { payloads } = require(payloadsUrl);
 const { siteId, expect, should, supertest, api, auth, sleep, error, testrail, runId, trTestRunCases } = require(common);
 const { updateTestCase } = require(testrailApi);
 
@@ -8,11 +10,6 @@ var resultStatus = 3;
 var resultComment = '';
 var testRunCaseId = '';
 
-var dateTime = new Date();
-var eventDate = dateTime.toISOString().split('Z')[0];
-var randString = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-
-
 describe('Mediaitem endpoints', function(){
     
     it('C45169426 It should be possible to Create a mediaItem', function(done){
@@ -20,54 +17,7 @@ describe('Mediaitem endpoints', function(){
 
       api.post('/v1/mediaitem/site/' + siteId)
           .set(auth)
-          .send(
-              {
-                  "archiveLogging": {
-                    "keywords": "Ivan",
-                    "language": "EN",
-                    "locationEditorial": "London",
-                    "reporter": "Stew"
-                  },
-                  "category": "Recording",
-                  "creationDate": dateTime,
-                  "creator": "Johnson",
-                  "crew": [
-                    {
-                      "name": "John Newman",
-                      "position": "Camera"
-                    }
-                  ],
-                  "description": "post mediatem test - dateTime",
-                  "details": "proxyless test",
-                  "editorialRights": {
-                    "restrictions": "Disney",
-                    "trafficLight": "GREEN"
-                  },
-                  "eventDate": eventDate,
-                  "externalDescription": "string",
-                  "frameRate": {
-                    "denominator": 1,
-                    "numerator": 60
-                  },
-                  "isAgency": true,
-                  "isBestMedia": true,
-                  "lengthFrames": 38072,
-                  "modifiedDate": dateTime,
-                  "outlet": "News",
-                  "pictureFormat": "16x9",
-                  "rights": {
-                    "copyright": "BSD",
-                    "restrictions": "WARNING THIS A TRAFFICLIGHT DESCRIPTION",
-                    "trafficLight": "GREEN"
-                  },
-                  "schemaVersion": "1-0-0",
-                  "soundFormat": "Mono",
-                  "source": "Morocco",
-                  "status": "Raw",
-                  "story": "zznttest",
-                  "type": "Edit"
-                }
-          )
+          .send(payloads[0])
           .expect('Content-Type', /json/)
           .end(async function (err, res){
             console.log(res.body);
@@ -112,47 +62,7 @@ describe('Mediaitem endpoints', function(){
   it('C1184295 It should be possible to edit the metadata of an item', function(done){
       api.put('/v1/mediaitem/site/' + siteId + '/item/4198')
           .set(auth)
-          .send(
-              {
-                "archiveLogging": {
-                    "keywords": randString,
-                    "language": "French- GB",
-                    "locationEditorial": "Perth",
-                    "reporter": "Jaden Smith"
-                },    
-                "category": "Package",
-                "crew": [
-                {
-                  "name": "Sir Arthur Conan Doyle",
-                  "position": "Author"
-                },
-                {
-                  "name": "JK Rowling",
-                  "position": "Author"
-                },
-                {
-                  "name": "Madam Carrie Lam",
-                  "position": "CEO"
-                }
-                ],
-                "description": "test amending item with string " + randString,
-                "details": "zzivanregression",
-                "eventDate": "2020-08-11T20:34:33.015",
-                "isBestMedia": true,
-                "outlet": "BBCNews",
-                "pictureFormat": "16x9",
-                "rights": {
-                    "copyright": "BSD",
-                    "restrictions": "NaN",
-                    "trafficLight": "RED"
-                },
-                "schemaVersion": "1-0-2",
-                "soundFormat": "Mono",
-                "source": "from Tooting",
-                "status": "Finished",
-                "story": "zzivanregression25-9-2020-13:48"
-            }
-          )
+          .send(payloads[1])
           .expect('Content-Type', /json/)
           .end(function (err, res){
             console.log(res.body);
---
+++ test/payloads/media-item-controller-test-payloads.js
@@ -0,0 +1,91 @@
+var dateTime = new Date();
+var eventDate = dateTime.toISOString().split('Z')[0];
+var randString = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+
+module.exports = {
+    payloads: [{
+        "archiveLogging": {
+          "keywords": "Ivan",
+          "language": "EN",
+          "locationEditorial": "London",
+          "reporter": "Stew"
+        },
+        "category": "Recording",
+        "creationDate": dateTime,
+        "creator": "Johnson",
+        "crew": [
+          {
+            "name": "John Newman",
+            "position": "Camera"
+          }
+        ],
+        "description": "post mediatem test - dateTime",
+        "details": "proxyless test",
+        "editorialRights": {
+          "restrictions": "Disney",
+          "trafficLight": "GREEN"
+        },
+        "eventDate": eventDate,
+        "externalDescription": "string",
+        "frameRate": {
+          "denominator": 1,
+          "numerator": 60
+        },
+        "isAgency": true,
+        "isBestMedia": true,
+        "lengthFrames": 38072,
+        "modifiedDate": dateTime,
+        "outlet": "News",
+        "pictureFormat": "16x9",
+        "rights": {
+          "copyright": "BSD",
+          "restrictions": "WARNING THIS A TRAFFICLIGHT DESCRIPTION",
+          "trafficLight": "GREEN"
+        },
+        "schemaVersion": "1-0-0",
+        "soundFormat": "Mono",
+        "source": "Morocco",
+        "status": "Raw",
+        "story": "zznttest",
+        "type": "Edit"
+      },
+      {
+        "archiveLogging": {
+            "keywords": randString,
+            "language": "French- GB",
+            "locationEditorial": "Perth",
+            "reporter": "Jaden Smith"
+        },    
+        "category": "Package",
+        "crew": [
+        {
+          "name": "Sir Arthur Conan Doyle",
+          "position": "Author"
+        },
+        {
+          "name": "JK Rowling",
+          "position": "Author"
+        },
+        {
+          "name": "Madam Carrie Lam",
+          "position": "CEO"
+        }
+        ],
+        "description": "test amending item with string " + randString,
+        "details": "zzivanregression",
+        "eventDate": "2020-08-11T20:34:33.015",
+        "isBestMedia": true,
+        "outlet": "BBCNews",
+        "pictureFormat": "16x9",
+        "rights": {
+            "copyright": "BSD",
+            "restrictions": "NaN",
+            "trafficLight": "RED"
+        },
+        "schemaVersion": "1-0-2",
+        "soundFormat": "Mono",
+        "source": "from Tooting",
+        "status": "Finished",
+        "story": "zzivanregression25-9-2020-13:48"
+    }]
+}
